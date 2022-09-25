86fbb15c5d0043a25a4ae81b43fb0c9a705ff9a2
Vladislav Ivanov <103313657+Vladisav2233@users.noreply.github.com>
2022-09-26 01:27:44 +0300

    Add files via upload

---
+++ ADO.NET_Lab/ADO.NET_Lab4.3_DataAdapter/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ADO.NET_Lab4._3_DataAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=IVANOV\\SQLEXPRESS;Integrated Security=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandText = "SELECT CustomerID, CompanyName\r\nFROM     Customers";
+            this.sqlCommand1.Connection = this.sqlConnection1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запрос данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ADO.NET_Lab/ADO.NET_Lab4.3_DataAdapter/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADO.NET_Lab4._3_DataAdapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Text.StringBuilder results = new System.Text.StringBuilder();
+            sqlCommand1.CommandType = CommandType.Text;
+            sqlConnection1.Open();
+            SqlDataReader reader = sqlCommand1.ExecuteReader();
+            bool MoreResults = false;
+            do
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        results.Append(reader[i].ToString() + "\t");
+                    }
+                    results.Append(Environment.NewLine);
+                }
+                MoreResults = reader.NextResult();
+            } while (MoreResults);
+            reader.Close();
+            sqlCommand1.Connection.Close();
+            
+        }
+    }
+}
