9268e9724fdae63669dba7dfae7b705162fb14a8
Vladislav Ivanov <103313657+Vladisav2233@users.noreply.github.com>
2022-09-26 01:24:37 +0300

    Add files via upload

---
+++ ADO.NET_Lab/ADO.NET_Lab4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ADO.NET_Lab4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NorthwindDataSet NorthwindDataset1 = new NorthwindDataSet();
+            NorthwindDataSetTableAdapters.CustomersTableAdapter CustomersTableAdapter1 = new NorthwindDataSetTableAdapters.CustomersTableAdapter();
+            CustomersTableAdapter1.Fill(NorthwindDataset1.Customers);
+            foreach (NorthwindDataSet.CustomersRow NWCustomer in NorthwindDataset1.Customers.Rows)
+            {
+                CustomersListBox.Items.Add(NWCustomer.CompanyName);
+            }
+
+        }
+    }
+}
